56f9c8af80da9b34032cf2ed56f96ea71dc7d1c9
Ubuntu <ubuntu@ip-172-31-17-210.us-west-2.compute.internal>
2017-03-04 18:08:24 +0000

    Added an ajax call using vanilla js

--- src/AppBundle/Resources/public/js/main.js
+++ src/AppBundle/Resources/public/js/main.js
@@ -1,3 +1,22 @@
 // main js
 
 console.log('Assetic is pulling in the JS');
+
+// ajax call to grab text
+function loadDoc(url, cFunction) {
+    var xhttp;
+    xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+// replace text on page
+function myFunction(xhttp) {
+    document.getElementById("demo").innerHTML =
+        xhttp.responseText;
+}
